c691f10c1e2bfb21d2bd9aa5311d04d3b0dc64e7
Amey Sunu <ameysunu00719@gmail.com>
2020-08-30 00:14:03 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/stive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Hexcolor('#F7D1FF'),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 0, 0),
+                  child: Text(
+                    "Greetings!",
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 35,
+                        color: Hexcolor('#0F9AC6')),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 2, 0, 0),
+                  child: Text(
+                    "So, what do you wanna do today?",
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 20,
+                        color: Hexcolor('#0F9AC6')),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 2, 0, 0),
+                    child: SizedBox(
+                      height: 300,
+                      width: 300,
+                      child: Image.asset('images/home.png'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: Card(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Image(
+                                image: NetworkImage(
+                                    'https://i.ibb.co/r3TXy9C/js.png')),
+                          ),
+                        ),
+                        Text('Learn JavaScript',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                color: Hexcolor('#000000'))),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: Card(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Image(
+                                image: NetworkImage(
+                                    'https://bashlogo.com/img/symbol/png/full_colored_dark.png')),
+                          ),
+                        ),
+                        Text('Learn Shell Scripting',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                color: Hexcolor('#000000'))),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: Card(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Image(
+                                image: NetworkImage(
+                                    'https://i.ibb.co/rthpwKV/kisspng-swift-apple-logo-objective-c-5b28a6c3b7f690-4346468915293907877535.png')),
+                          ),
+                        ),
+                        Text('Learn Swift and iOS App Development',
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                color: Hexcolor('#000000'))),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: Text(
+                          "Don't want to learn the above languages? Click me.",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 18,
+                              color: Hexcolor('#008B81')),
+                        ),
+                      ),
+                      SizedBox(
+                          height: 80,
+                          width: 80,
+                          child: Image.asset('images/hello.png'))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
