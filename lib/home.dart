a73f50f92c3e06506bab110ff966f5040c35faca
Amey Sunu <ameysunu00719@gmail.com>
2020-08-30 13:31:30 +0300

    achievement added

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'recommend.dart';
+import 'package:achievement_view/achievement_view.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +9,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void done(BuildContext context) {
+    AchievementView(
+      context,
+      title: 'Yasssss',
+      subTitle: 'We have added this to your list.',
+      textStyleTitle: TextStyle(fontFamily: 'Poppins', color: Colors.black),
+      textStyleSubTitle: TextStyle(fontFamily: 'Poppins', color: Colors.black),
+      icon: Icon(
+        Icons.code,
+        color: Colors.black,
+      ),
+      borderRadius: 25.0,
+      color: Hexcolor('#FFC7CE'),
+    )..show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,74 +68,88 @@ class _HomeState extends State<Home> {
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Card(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: Image(
-                                image: NetworkImage(
-                                    'https://i.ibb.co/r3TXy9C/js.png')),
-                          ),
+                  child: InkWell(
+                      child: Card(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: SizedBox(
+                                height: 30,
+                                width: 30,
+                                child: Image(
+                                    image: NetworkImage(
+                                        'https://i.ibb.co/r3TXy9C/js.png')),
+                              ),
+                            ),
+                            Text('Learn JavaScript',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 15,
+                                    color: Hexcolor('#000000'))),
+                          ],
                         ),
-                        Text('Learn JavaScript',
-                            style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 15,
-                                color: Hexcolor('#000000'))),
-                      ],
+                      ),
+                      onTap: () {
+                        done(context);
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: InkWell(
+                    child: Card(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: Image(
+                                  image: NetworkImage(
+                                      'https://bashlogo.com/img/symbol/png/full_colored_dark.png')),
+                            ),
+                          ),
+                          Text('Learn Shell Scripting',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 15,
+                                  color: Hexcolor('#000000'))),
+                        ],
+                      ),
                     ),
+                    onTap: () {
+                      done(context);
+                    },
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Card(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: Image(
-                                image: NetworkImage(
-                                    'https://bashlogo.com/img/symbol/png/full_colored_dark.png')),
+                  child: InkWell(
+                    child: Card(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: Image(
+                                  image: NetworkImage(
+                                      'https://i.ibb.co/rthpwKV/kisspng-swift-apple-logo-objective-c-5b28a6c3b7f690-4346468915293907877535.png')),
+                            ),
                           ),
-                        ),
-                        Text('Learn Shell Scripting',
-                            style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 15,
-                                color: Hexcolor('#000000'))),
-                      ],
+                          Text('Learn Swift and iOS App Development',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 15,
+                                  color: Hexcolor('#000000'))),
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Card(
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: Image(
-                                image: NetworkImage(
-                                    'https://i.ibb.co/rthpwKV/kisspng-swift-apple-logo-objective-c-5b28a6c3b7f690-4346468915293907877535.png')),
-                          ),
-                        ),
-                        Text('Learn Swift and iOS App Development',
-                            style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 15,
-                                color: Hexcolor('#000000'))),
-                      ],
-                    ),
+                    onTap: () {
+                      done(context);
+                    },
                   ),
                 ),
                 InkWell(
@@ -129,7 +160,7 @@ class _HomeState extends State<Home> {
                         Container(
                           width: MediaQuery.of(context).size.width * 0.7,
                           child: Text(
-                            "Don't want to learn the above languages? Click me.",
+                            "Want to find something new? Tap me",
                             style: TextStyle(
                                 fontFamily: 'Poppins',
                                 fontSize: 18,
